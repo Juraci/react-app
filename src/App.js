363f3dafd152c69d720c41ab3eed9d50e4c1fc00
Juraci Vieira <juraci.vieira@gmail.com>
2017-06-22 19:01:16 -0300

    event tracker

--- src/App.js
+++ src/App.js
@@ -17,8 +17,43 @@ class App extends React.Component {
   render() {
     return(
       <div>
-        <h1>{this.state.txt}</h1>
-        <Widget update={this.update.bind(this)} />
+      <h1>{this.state.txt}</h1>
+      <Widget update={this.update.bind(this)} />
+      <EventTracker />
+      </div>
+    );
+  }
+}
+
+class EventTracker extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentEvent: '---'
+    };
+
+    this.update = this.update.bind(this);
+  }
+
+  update(e) {
+    this.setState({ currentEvent: e.type });
+  }
+
+  render() {
+    return(
+      <div style={{ marginTop: '10px' }}>
+        <textarea
+          onKeyPress={this.update}
+          onCopy={this.update}
+          onPaste={this.update}
+          onCut={this.update}
+          onFocus={this.update}
+          onBlur={this.update}
+          onDoubleClick={this.update}
+          cols='30'
+          rows='10'
+        />
+        <h1>{this.state.currentEvent}</h1>
       </div>
     );
   }
